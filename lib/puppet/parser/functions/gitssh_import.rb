1970e7c472795042ff0692cda6dd9a17a26b6788
Justice London <justice@ministrycentered.com>
2015-10-22 11:21:11 -0700

    Fix issue with how many arguments the function can accept.

--- lib/puppet/parser/functions/gitssh_import.rb
+++ lib/puppet/parser/functions/gitssh_import.rb
@@ -3,9 +3,9 @@ require 'net/http'
 module Puppet::Parser::Functions
   newfunction(:gitssh_import, :type => :rvalue) do |args|
 
-    if (args.size != 1) then
+    if (args.size != 3) then
       raise(Puppet::ParseError, "gitssh_import(): Wrong number of arguments " +
-        "given #{args.size} for 1")
+        "given #{args.size} for 3")
     end
 
     #Function accepts an array of usernames as input
